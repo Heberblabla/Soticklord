acbabbd1d8de00578de665ea68249934080751f0
heber <heberantoniocondoriyana@gmail.com>
2025-09-30 22:53:15 -0500

    Waos

--- app/src/main/java/com/waos/soticklord/Perfil.kt
+++ app/src/main/java/com/waos/soticklord/Perfil.kt
@@ -5,14 +5,21 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_perfil)
-
+        //elimina las barras de tareas
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         // Ajuste de barras del sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -23,6 +30,8 @@ class Perfil : AppCompatActivity() {
         // 🔑 Recibir datos que mandaste desde Iniciar_Sesion
         val nombreUsuario = intent.getStringExtra("NOMBRE_USUARIO")
         val contraseña = intent.getStringExtra("PASSWORD_USUARIO")
+
+
 
     }
 }
